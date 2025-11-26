256d515e43ed391e7ad53bac9ca15ef623cd5f7f
DicoJhor <jhordan.vt11@gmail.com>
2025-11-26 13:28:59 -0500

    BD

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'hopper.proxy.rlwy.net',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'facturador_multiempresa',
+  password: process.env.DB_PASSWORD || 'CdsUaWLlernoPGVAFHKnpUWmwWblxTYm',
+  database: process.env.DB_NAME || 'railway1',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
