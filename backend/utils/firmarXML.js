d4f35ca1ae674bb166c144233cb530d3803b8c92
DicoJhor <jhordan.vt11@gmail.com>
2025-12-02 19:52:22 -0500

    Update firmarXML.js

--- backend/utils/firmarXML.js
+++ backend/utils/firmarXML.js
@@ -2,8 +2,8 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { SignedXml } from "xml-crypto";
 import forge from "node-forge";
-import crypto from "crypto";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,75 +26,60 @@ export const firmarXML = (xmlPath, xmlContent) => {
     const pfxBuffer = fs.readFileSync(pfxPath);
     const { privateKeyPem, certPem } = pemFromPfx(pfxBuffer, process.env.SUNAT_CERT_PASS || "");
     
-    // 🔧 PASO 1: Crear el hash del XML ORIGINAL (sin firma)
-    const hash = crypto.createHash('sha256').update(xmlContent, 'utf8').digest('base64');
-    
-    // 🔧 PASO 2: Crear SignedInfo
-    const signedInfo = `<SignedInfo xmlns="http://www.w3.org/2000/09/xmldsig#">
-  <CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"/>
-  <SignatureMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"/>
-  <Reference URI="">
-    <Transforms>
-      <Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/>
-    </Transforms>
-    <DigestMethod Algorithm="http://www.w3.org/2001/04/xmlenc#sha256"/>
-    <DigestValue>${hash}</DigestValue>
-  </Reference>
-</SignedInfo>`;
-    
-    // 🔧 PASO 3: Firmar el SignedInfo usando canonicalización
-    const canonicalSignedInfo = signedInfo
-      .replace(/\n/g, '')
-      .replace(/>\s+</g, '><')
-      .trim();
-    
-    const sign = crypto.createSign('RSA-SHA256');
-    sign.update(canonicalSignedInfo);
-    const signature = sign.sign(privateKeyPem, 'base64');
-    
-    // 🔧 PASO 4: Extraer el certificado sin headers
+    // Extraer el certificado sin headers
     const certBase64 = certPem
       .replace(/-----BEGIN CERTIFICATE-----/, '')
       .replace(/-----END CERTIFICATE-----/, '')
-      .replace(/\n/g, '');
+      .replace(/\n/g, '')
+      .replace(/\r/g, '');
     
-    // 🔧 PASO 5: Construir la firma completa
-    const signatureXml = `
-  <ds:Signature xmlns:ds="http://www.w3.org/2000/09/xmldsig#" Id="SignatureSP">
-    <ds:SignedInfo>
-      <ds:CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"/>
-      <ds:SignatureMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"/>
-      <ds:Reference URI="">
-        <ds:Transforms>
-          <ds:Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/>
-        </ds:Transforms>
-        <ds:DigestMethod Algorithm="http://www.w3.org/2001/04/xmlenc#sha256"/>
-        <ds:DigestValue>${hash}</ds:DigestValue>
-      </ds:Reference>
-    </ds:SignedInfo>
-    <ds:SignatureValue>${signature}</ds:SignatureValue>
-    <ds:KeyInfo>
-      <ds:X509Data>
-        <ds:X509Certificate>${certBase64}</ds:X509Certificate>
-      </ds:X509Data>
-    </ds:KeyInfo>
-  </ds:Signature>`;
+    // Crear firma XML usando xml-crypto
+    const sig = new SignedXml();
     
-    // 🔧 PASO 6: Insertar la firma en la sección ExtensionContent
-    const signedXml = xmlContent.replace(
-      /<ext:ExtensionContent>\s*<\/ext:ExtensionContent>/,
-      `<ext:ExtensionContent>${signatureXml}
-      </ext:ExtensionContent>`
+    // Configurar la clave privada
+    sig.signingKey = privateKeyPem;
+    
+    // Configurar algoritmos
+    sig.signatureAlgorithm = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
+    sig.canonicalizationAlgorithm = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
+    
+    // Agregar referencia al documento completo
+    sig.addReference(
+      "//*[local-name(.)='Invoice']", // XPath al nodo Invoice
+      [
+        "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
+        "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+      ],
+      "http://www.w3.org/2001/04/xmlenc#sha256"
     );
     
-    // 🔧 PASO 7: Guardar el XML firmado
-    const signedPath = xmlPath;
+    // Configurar KeyInfo con el certificado
+    sig.keyInfoProvider = {
+      getKeyInfo: () => {
+        return `<X509Data><X509Certificate>${certBase64}</X509Certificate></X509Data>`;
+      }
+    };
     
-    fs.writeFileSync(signedPath, signedXml, "utf8");
+    // Calcular la firma
+    sig.computeSignature(xmlContent, {
+      location: { reference: "//*[local-name(.)='ExtensionContent']", action: "append" }
+    });
     
-    console.log(`🔐 XML firmado y guardado en: ${path.basename(signedPath)}`);
+    // Obtener el XML firmado
+    const signedXml = sig.getSignedXml();
     
-    return signedPath;
+    // Agregar el atributo Id="SignatureSP" a la firma
+    const finalXml = signedXml.replace(
+      /<Signature xmlns="http:\/\/www.w3.org\/2000\/09\/xmldsig#">/,
+      '<Signature xmlns="http://www.w3.org/2000/09/xmldsig#" Id="SignatureSP">'
+    );
+    
+    // Guardar el XML firmado
+    fs.writeFileSync(xmlPath, finalXml, "utf8");
+    
+    console.log(`🔐 XML firmado correctamente con xml-crypto`);
+    
+    return xmlPath;
   } catch (error) {
     console.error("❌ Error al firmar XML:", error);
     throw error;
